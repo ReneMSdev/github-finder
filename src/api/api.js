457b85aa893de91e7a18d37b1516bb54473f4a0f
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-03 12:49:03 +0700

    UserList Component

---
+++ src/api/api.js
@@ -0,0 +1,2 @@
+export const githubUrl = import.meta.env.VITE_GITHUB_URL
+export const githubToken = import.meta.env.VITE_GITHUB_TOKEN
---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { githubUrl, githubToken } from '../../api/api'
+
+const UserResults = () => {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${githubUrl}/users`, {
+      headers: {
+        Authorization: `token ${githubToken}`,
+      },
+    })
+
+    const data = await response.json()
+
+    setUsers(data)
+    setLoading(false)
+  }
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-col-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    )
+  } else {
+    return <h3>Loading...</h3>
+  }
+}
+export default UserResults
