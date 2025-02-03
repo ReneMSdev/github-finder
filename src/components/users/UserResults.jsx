187aed623b720aa674ccdc61b7e3e230bf68c4cc
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-03 22:40:59 +0700

    updated UserResults, added UserItem Component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { githubUrl, githubToken } from '../../api/api'
 import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
 
 const UserResults = () => {
   const [users, setUsers] = useState([])
@@ -27,7 +28,7 @@ const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-col-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
