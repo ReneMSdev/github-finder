9817cfb77e71c9c9c7f15086d7c6d4644cfbe576
renesalomone <108975981+renesalomone@users.noreply.github.com>
2025-02-04 13:45:49 +0700

    Alert Context & Reducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,30 @@ import About from './pages/About'
 import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { GithubProvider } from './context/github/GithubContext.jsx'
+import { AlertProvider } from './context/alert/AlertContext.jsx'
 
 const App = () => {
   return (
-    <div className="flex flex-col justify-start h-screen">
-      <Navbar />
-      <main className="flex-grow container mx-auto px-3">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/notfound" element={<NotFound />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className="flex flex-col justify-start h-screen">
+            <Navbar />
+            <main className="flex-grow container mx-auto px-3">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<NotFound />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
+    </GithubProvider>
   )
 }
 export default App
